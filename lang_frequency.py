0f87dc911d15a4565aa60809759ae794db764539
SkayLarsen <SkayLarsen@gmail.com>
2017-01-03 22:27:56 +0300

    задача #5

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,26 @@
+import os.path
+import sys
+import string
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as text_file:
+            return text_file.read()
+    else:
+        return None
 
 
 def get_most_frequent_words(text):
-    pass
+    return Counter(text.split()).most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    text_data = load_data(sys.argv[1]).replace('\n', ' ').translate(
+        str.maketrans({a: None for a in string.punctuation}))
+    if text_data:
+        for word in get_most_frequent_words(text_data):
+            print(word[0], word[1])
+    else:
+        print("Не удалось прочитать файл")
